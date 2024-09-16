00eb51455b05b8f09f2c34b259db6958c55f5b8a
jf-lacerda <joao.sclf@gmail.com>
2024-09-16 20:10:24 -0300

    first commit

---
+++ aula5.py
@@ -0,0 +1,20 @@
+# conversão de tipos, coerção
+# type convertion, typecasting, coercion
+# é o ato de converter um tipo em outro
+# tipos imutaveus e primitivos
+# str, int, float, bool
+
+#
+print(int('1'), type(1))
+
+# Fazer a conversão para número inteiro
+print(int('1') + 1)
+
+# Fazer a conversão para floot
+print(float('1') + 1)
+
+# Fazer a conversão para bool
+print(bool(''))
+
+# Fazer a conversão para string
+print(str(5) + 'g')
