3a484ce46c3208cdb586ea458984a49897d762f7
João Lacerda <joao.sclf@gmail.com>
2024-09-30 16:29:16 -0300

    aula-13

---
+++ aula13.py
@@ -0,0 +1,10 @@
+nome = 'João Lacerda'
+altura = 1.80
+peso = 88
+imc = peso / (altura * altura)
+
+linha_1 = f'{nome} tem {altura:.2f}m de altura, pesa {peso}kg e o seu imc é {imc:.1f}'
+
+
+print(linha_1)
+
